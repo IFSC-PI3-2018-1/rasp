02e6d5504cf875dff522f1c0d2d4f27ff132f183
Pedro Hames <pedro.hamess@gmail.com>
2018-06-08 20:00:59 -0300

    Adicionando exemplos com WEBSOCKET

--- rasp.py
+++ rasp.py
@@ -7,22 +7,34 @@ Created on Fri Jun  1 17:23:04 2018
 
 import base64
 import requests
-import json
+#import json
 import color_figure_detector
+import asyncio
+import websockets
 
-
-def __handle(self):
-    ##Processamento CV2
+def start_server(self):
+    colors = str(color_figure_detector.get_colors())
+    r = requests.post('http://localhost:5000/postacor', data={'lista':colors})
+    websockets.serve(_handle, 'localhost', 20181)
+    
+async def _handle(websocket, path):
+    
+    req = await websocket.recv()
+    ##GET IMAGEM
+    
+    color_figure_detector.main('Image/testecores.jpg', req[1])
     
     with open("Image/testecores.jpg", "rb") as image_file:
         encoded_string = base64.b64encode(image_file.read())
         
-    print(type(encoded_string))
     
-    payload = {'nID':'3','id_facebook':'12345','origem':'rasp','conteudo':encoded_string.decode('utf-8')}
-    print(type(payload))
+    payload = {'nID':req[0],'origem':'rasp','conteudo':encoded_string.decode('utf-8')}
     
     r = requests.post('http://localhost:5000/imagens_enviadas', json=payload)
-    print(r.status_code, r.reason)
+#    print(r.status_code, r.reason)
+    
+    
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()   
 
 
