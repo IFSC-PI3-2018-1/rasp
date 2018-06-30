7bff9161f1f8d5fcd950d2b29a4ebad79a3c9fcb
Pedro Hames <pedro.hamess@gmail.com>
2018-06-30 09:22:21 -0300

    MVP2

--- rasp.py
+++ rasp.py
@@ -7,34 +7,76 @@ Created on Fri Jun  1 17:23:04 2018
 
 import base64
 import requests
-#import json
 import color_figure_detector
-import asyncio
-import websockets
+import zmq
+import wget
+import time
 
-def start_server(self):
+#FPGA = 'https://wiki.sj.ifsc.edu.br/wiki/images/3/3f/FotoAntena-EngTelecom.jpg'
+FPGA = 'http://www.imagenspng.com.br/wp-content/uploads/2015/02/super-mario-mario-01.png'
+WebService = 'http://localhost:5000'
+
+# ANTENA IFSC
+#https://wiki.sj.ifsc.edu.br/wiki/images/3/3f/FotoAntena-EngTelecom.jpg
+def start_rasp():
     colors = str(color_figure_detector.get_colors())
-    r = requests.post('http://localhost:5000/postacor', data={'lista':colors})
-    websockets.serve(_handle, 'localhost', 20181)
-    
-async def _handle(websocket, path):
-    
-    req = await websocket.recv()
-    ##GET IMAGEM
-    
-    color_figure_detector.main('Image/testecores.jpg', req[1])
-    
-    with open("Image/testecores.jpg", "rb") as image_file:
-        encoded_string = base64.b64encode(image_file.read())
-        
-    
-    payload = {'nID':req[0],'origem':'rasp','conteudo':encoded_string.decode('utf-8')}
-    
-    r = requests.post('http://localhost:5000/imagens_enviadas', json=payload)
-#    print(r.status_code, r.reason)
-    
-    
-asyncio.get_event_loop().run_until_complete(start_server)
-asyncio.get_event_loop().run_forever()   
+    r = requests.post(WebService + '/post_cor', json={'lista':colors})
+    if(r):
+        print('Cores disponíveis enviadas ao servidor')
+    port = "20181"
+    context = zmq.Context()
+    socket = context.socket(zmq.SUB)
+    socket.connect ("tcp://localhost:%s" % port)
+    return socket
 
+def getImage(url, requestId):
+    path = '/tmp/'+ requestId + '.png'
+    wget.download(url, path)
+    return path
 
+def imageProcessing(path,cor):
+    lcor = []
+    lcor.append(cor)
+    color_figure_detector.main(path,lcor)
+    
+def imageToBase64(path):
+    with open(path, "rb") as image_file:
+        Ibase64 = base64.b64encode(image_file.read())
+        return Ibase64
+    
+def sendImage(imagem,requestId):
+    print('============================================')
+    print(type(imagem))
+    print(len(imagem))
+    r = requests.post(WebService + '/enviar_imagem', json={'requestId':requestId,
+                                                           'origem':'rasp',
+                                                           'imagem': imagem,
+                                                           'timestamp': str(time.time())})
+    if(r):
+        return True
+    else:
+        return False
+
+if __name__ == "__main__":
+    print('RASP INICIADA')
+    socket = start_rasp()
+    while(True):
+        topic = 'requeststorasp'
+        socket.setsockopt_string(zmq.SUBSCRIBE, topic)
+        string = socket.recv()
+        print('============= REQUISICAO RECEBIDA ===============')
+        print(string)
+        cor, requestId = string.split()[-2:]
+        cor = cor.decode()
+        requestId = requestId.decode()
+        print(cor,requestId)
+        path = getImage(FPGA, requestId)
+        imageProcessing(path, cor)
+        encodedImage = imageToBase64(path)
+        if(sendImage(encodedImage, requestId)):
+            print('Sucesso na requisição: ' + requestId)
+        else:
+            print('Falha ao atender requisição: ' + requestId)        
+    
+    
+    
